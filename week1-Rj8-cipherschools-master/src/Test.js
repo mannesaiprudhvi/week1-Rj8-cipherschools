52e03ce48b7403c5a1e3ded8251deba5720d7481
mannesaiprudhvi <chowdaryprudhvi1046@gmail.com>
2023-01-27 22:12:05 +0530

    RJ8

---
+++ week1-Rj8-cipherschools-master/src/Test.js
@@ -0,0 +1,21 @@
+import React from "react";
+{/*const Test =() => {
+  return (
+    <div>
+// <h1>hiii prudhvi</h1>
+    </div>
+)
+} */}
+
+class Test extends React.Component{
+    render () {
+        return (
+            <div>
+            <h1 id="testId">Hello i am prudhvi</h1>
+            </div>
+        );  
+    }
+}
+
+
+export default Test;
